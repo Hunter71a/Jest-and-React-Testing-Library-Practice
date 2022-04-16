4c3a0f755c6e095b856d6188d3616a4f25a27e22
Carl Olson <colson9@student.cscc.edu>
2022-04-16 18:39:30 -0400

    add innitial tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,23 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('button has correct initial color', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+
+  // find an element with the role of a button and text of 'Change to blue'
+  const colorButton = screen.getByRole('button', { name: /Change to blue/i });
+
+  //expect the background color to be red
+  expect(colorButton).toHaveStyle({ backgroundColor: 'red' });
+});
+
+test('button has correct initial text', () => {});
+
+test('button turns blue when clicked', () => {});
+
+/* test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByRole('link', { name: /hello react/i });
   expect(linkElement).toBeInTheDocument(); // assertion -- causes the test to succeed or fail
 });
 
@@ -12,3 +26,7 @@ test('renders hello react link', () => {
   const linkElement = screen.getByText(/hello react/i);
   expect(linkElement).toBeInTheDocument(); // assertion -- causes the test to succeed or fail
 });
+ */
+/* test('renders something else', () => {
+  throw new Error('Shut up, CHUMP!');
+}); */
