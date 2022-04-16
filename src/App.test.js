8d015ffac0b16f7e1a87d5659b1c9b7675556ce7
Carl Olson <colson9@student.cscc.edu>
2022-04-16 17:11:32 -0400

    update changes

--- src/App.test.js
+++ src/App.test.js
@@ -4,5 +4,11 @@ import App from './App';
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  expect(linkElement).toBeInTheDocument(); // assertion -- causes the test to succeed or fail
+});
+
+test('renders hello react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/hello react/i);
+  expect(linkElement).toBeInTheDocument(); // assertion -- causes the test to succeed or fail
 });
